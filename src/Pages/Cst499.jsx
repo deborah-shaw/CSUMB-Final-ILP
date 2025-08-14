a6763501e103e81b30a7187d05b8f6c66010ffe1
mkt <122516715+deborah-shaw@users.noreply.github.com>
2025-08-14 15:58:54 -0700

    update cst499

--- src/Pages/Cst499.jsx
+++ src/Pages/Cst499.jsx
@@ -15,7 +15,21 @@ function Cst499(){
                 <p>Students will work on a project in large groups (up to 5 students in each group), developing requirements specification, a solution plan followed by design and implementation of the solution. The problem statement for the projects will be selected by the faculty. Faculty will also play the role of a project manager directing the schedule and deliverables for these projects.</p>
 
                 <h3>Projects</h3>
-                <p>Coming Soon</p>
+                
+                <p>Presentation</p>
+
+                {/* Example MP4 embed */}
+                <video width="800" controls autoPlay={false} muted>
+                    <source src="/Personal-Website-React-Vite/projects/cst499_Presentation_Draft.mp4" type="video/mp4" />
+                    Your browser does not support the video tag.
+                </video>
+
+                <p>Update and Testing Report</p>
+                
+                <iframe src="/Personal-Website-React-Vite/projects/cst499_Status_Update_and_Testing.pdf" width="100%" height="600px">
+                    This browser does not support PDFs. Please download the PDF instead:
+                    <a href="/Personal-Website-React-Vite/projects/cst499_Status_Update_and_Testing.pdf">Download PDF</a>
+                </iframe>
             </div>
             <Footer/>
         </>
